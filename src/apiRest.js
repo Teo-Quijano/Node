5f2197eb0323793cadaa4d5b5dd9f9fba52e7a78
Teo-Quijano <105201972+Teo-Quijano@users.noreply.github.com>
2022-08-16 14:13:57 +0100

    Delete src directory

--- src/apiRest.js
+++
@@ -1,6 +0,0 @@
-
-const app = require("./app")
-
-app.listen(app.get("port"), function () {
-  console.log("Servidor escuchando en el Puerto" + app.get("port"))
-})
--- src/error/errorHandling.js
+++
@@ -1,6 +0,0 @@
-
-function errorHandling(err, request, response, next) {
-  response.status(500).json({ message: err.message });
-}
-
-module.exports = errorHandling;
