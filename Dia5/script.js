6bfd4b7b7f2526e10a620f1f6f5778c0eb215ba8
Teo-Quijano <105201972+Teo-Quijano@users.noreply.github.com>
2022-08-18 12:28:50 +0100

    Update retos Node

--- Dia5/script.js
+++ Dia5/script.js
@@ -1,53 +1,45 @@
+let name = document.getElementById("FAV").value;
+let lista = document.getElementById("listaPokemon");
 
-        let name = document.getElementById('FAV').value;
-        let lista = document.getElementById("listaPokemon")
-
-        function consultarPokemon(name, number) {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        .then(function (response) {
-            response.json()
-            .then(function (pokemon) {
-                crearPokemon(pokemon, number)
-               
-        })
-        })}
-
-        function rivalPokemon(id, number) {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        .then(function (response) {
-            response.json()
-            .then(function (pokemon) {
-                crearPokemon(pokemon, number)
-        })
-        })}
-
-
-        function getName() {
-        name = document.getElementById('FAV').value;
-        consultarPokemon(name, 1)
-       
-    }
-
-        function peleaDePokemones() {
-        let rival = Math.round(Math.random() * 150)
-         
-        rivalPokemon(rival, 2)
+function consultarPokemon(name, number) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then(function (response) {
+    response.json().then(function (pokemon) {
+      crearPokemon(pokemon, number);
+    });
+  });
 }
 
-        function crearPokemon(pokemon, number) {
+function rivalPokemon(id, number) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(function (response) {
+    response.json().then(function (pokemon) {
+      crearPokemon(pokemon, number);
+    });
+  });
+}
 
-       let item = lista.querySelector(`#pokemon-${number}`)
-            let imagen = item.getElementsByTagName("img")[0]
-            imagen.setAttribute("src", pokemon.sprites.front_shiny)
-            let nombre = item.getElementsByTagName("h3")[0]
-            nombre.textContent = pokemon.name
-            let peso = item.getElementsByTagName("h4")[0]
-            peso.textContent = pokemon.weight
-            let tipo = item.getElementsByTagName("h5")[0]
-            tipo.textContent = pokemon.types[0].type.name
-            let habilidad = item.getElementsByTagName("h6")[0]
-            habilidad.textContent = pokemon.abilities[0].ability.name
-   
-        }
+function getName() {
+  name = document.getElementById("FAV").value;
+  consultarPokemon(name, 1);
+}
 
-        peleaDePokemones();
+function peleaDePokemones() {
+  let rival = Math.round(Math.random() * 150);
+
+  rivalPokemon(rival, 2);
+}
+
+function crearPokemon(pokemon, number) {
+  let item = lista.querySelector(`#pokemon-${number}`);
+  let imagen = item.getElementsByTagName("img")[0];
+  imagen.setAttribute("src", pokemon.sprites.front_shiny);
+  let nombre = item.getElementsByTagName("h3")[0];
+  nombre.textContent = pokemon.name;
+  let peso = item.getElementsByTagName("h4")[0];
+  peso.textContent = pokemon.weight;
+  let tipo = item.getElementsByTagName("h5")[0];
+  tipo.textContent = pokemon.types[0].type.name;
+  let habilidad = item.getElementsByTagName("h6")[0];
+  habilidad.textContent = pokemon.abilities[0].ability.name;
+}
+
+peleaDePokemones();
